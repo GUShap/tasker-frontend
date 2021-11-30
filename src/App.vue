c9d633cc074273fd567bd7e8561d3bd0249f02f5
SundosGutty <sundosgutty@gmail.com>
2021-11-30 15:20:21 +0200

    home page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
-      <router-link to="/tasker-app">Tasker</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <h1>Logo</h1>
+      <div class="main-nav">
+        <router-link to="/">Product</router-link>
+        <router-link to="/about">Solutions</router-link>
+        <router-link to="/tasker-app">Templates</router-link>
+        <router-link to="/tasker-app">Pricing</router-link>
+      </div>
+      <div class="">
+         <router-link to="/tasker-app">Login</router-link>
+          <router-link to="/tasker-app">Sign In</router-link>
+
+      </div>
+    </header>
+    <router-view />
   </div>
 </template>
 
