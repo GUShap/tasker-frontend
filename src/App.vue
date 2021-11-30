8b7354c76e485704dc89fd7b8a568a75cb2ac9f7
SundosGutty <sundosgutty@gmail.com>
2021-11-30 15:51:13 +0200

    styling home page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <header>
-      <h1>Logo</h1>
+    <header class="home-header">
+      <h1>Tasker</h1>
       <div class="main-nav">
         <router-link to="/">Product</router-link>
         <router-link to="/about">Solutions</router-link>
-        <router-link to="/tasker-app">Templates</router-link>
-        <router-link to="/tasker-app">Pricing</router-link>
+        <router-link to="/home">Templates</router-link>
+        <router-link to="/home">Pricing</router-link>
       </div>
-      <div class="">
-         <router-link to="/tasker-app">Login</router-link>
-          <router-link to="/tasker-app">Sign In</router-link>
-
+      <div class="btn-container">
+        <router-link to="/home">Login</router-link>
+        <router-link to="/home">Sign In</router-link>
       </div>
     </header>
     <router-view />
