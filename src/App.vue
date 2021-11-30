3f9a420225d08da67310edeb416eda3408818290
SundosGutty <sundosgutty@gmail.com>
2021-11-30 16:23:36 +0200

    home page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-    <header class="home-header">
+    <header class="landing-header">
+      <div>
       <h1>Tasker</h1>
-      <div class="main-nav">
+      <div class="landing--nav">
         <router-link to="/">Product</router-link>
         <router-link to="/about">Solutions</router-link>
         <router-link to="/home">Templates</router-link>
         <router-link to="/home">Pricing</router-link>
       </div>
-      <div class="">
-         <router-link to="/tasker-app">Login</router-link>
-         <router-link to="/home">start</router-link>
-          <router-link to="/tasker-app">Sign In</router-link>
-
+      </div>
+      <div class="action-btn">
+        <router-link to="/tasker-app">Login</router-link>
+        <router-link to="/tasker-app">Sign In</router-link>
+        <router-link to="/tasker-app">Get Started</router-link>
       </div>
     </header>
-      <h1>landing page</h1>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import "./styles/styles.scss"
 import boardStore from './store/board.store'
+import ElementUI from 'element-ui';
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   router,
