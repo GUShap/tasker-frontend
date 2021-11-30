18c5103e8ae7cc5868a3d54102f96856d8719407
ishaynitzan <ishaynitzan@gmail.com>
2021-11-30 13:21:45 +0200

    first database

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <pre>
+      {{board}}
+    </pre>
   </div>
 </template>
+
+<script>
+
+import {board} from '../../database.js';
+
+export default {
+  setup() {
+    
+  },
+  data(){
+    return{
+      board : board
+    }
+  }
+}
+</script>
