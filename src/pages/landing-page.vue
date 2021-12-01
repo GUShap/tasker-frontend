75b9554f473d7be9d12f8c10d09d6e2c7ce191c8
SundosGutty <sundosgutty@gmail.com>
2021-12-01 21:28:10 +0200

    landing-page

--- src/pages/landing-page.vue
+++ src/pages/landing-page.vue
@@ -1,29 +1,28 @@
 <template>
-  <section id="app">
-    <header class="home-header align-center main-container">
-      <section class="">
-      <div>Tasker</div>
-      <div class="home-nav">
-        <router-link to="/">Product</router-link> |
-        <router-link to="/about">Solutions</router-link> |
-        <router-link to="/home">Templates</router-link> |
-        <router-link to="/home">Pricing</router-link> |
-      </div>
+  <section id="app" class="landing-page">
+    <header class="home-header align-center">
+      <section class="home-nav">
+        <a class="logo-home">Tasker</a>
+        <div class="extra-info">
+          <router-link to="/">Product</router-link>
+          <router-link to="/about">Solutions</router-link>
+          <router-link to="/home">Templates</router-link>
+          <router-link to="/home">Pricing</router-link>
+        </div>
       </section>
       <div class="action-btn">
-        <router-link to="/login">Login</router-link> |
-        <router-link to="/home">start</router-link> |
-        <router-link to="/login">Sign In</router-link> |
+        <router-link to="/login">Login</router-link>
+        <router-link class="get-started" to="/home"> Get Started</router-link>
       </div>
     </header>
     <section class="info-section">
       <section class="info-header">
-        <h1>Work Without Limits</h1>
-        <h5>
+        <h1 class="info-heading">Work Without Limits</h1>
+        <h5 class="opening-para">
           Easily build, run, and scale your dream workflows on one platform.
           What would you like to manage with wednesday Work OS?
         </h5>
-        <button>Get Started ></button>
+        <button>Get Started<i class="fas fa-chevron-right"></i></button>
         <!-- <img> -->
       </section>
       <section class="more-info">
