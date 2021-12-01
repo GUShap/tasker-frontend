b619e59a592cd51c7643e307cf5b0b4698e72759
SundosGutty <sundosgutty@gmail.com>
2021-12-02 01:53:23 +0200

    final landing page design

--- src/pages/landing-page.vue
+++ src/pages/landing-page.vue
@@ -22,35 +22,44 @@
           Easily build, run, and scale your dream workflows on one platform.
           What would you like to manage with wednesday Work OS?
         </h5>
-        <button>Get Started<i class="fas fa-chevron-right"></i></button>
+        <router-link class="get-started-in" to="/home">
+          Get Started<i class="fas fa-chevron-right"></i
+        ></router-link>
       </section>
-      <!-- <img> -->
-      <section class="more-info">
-        <h3>Manage everything in one workspace</h3>
-        <p>
-          Planning, tracking and delivering your team's best work has never been
-          easier
-        </p>
-        <img :src="require(`@/pics/pic1.png`)" />
-      </section>
-      <section class="more-info">
-        <h3>Visualize work with Views</h3>
-        <p>View data as a kanban, danban, dashboard and more</p>
-        <!-- <img> -->
+      <img class="main-img" :src="require(`@/pics/picture1.jpeg`)" />
+      <section class="more-info first-p">
+        <img :src="require(`@/pics/picture2.png`)" />
+        <div class="txt">
+          <h3>Manage everything in one workspace</h3>
+          <p>
+            Planning, tracking and delivering your team's best work has never been easier
+          </p>
+        </div>
       </section>
       <section class="more-info">
-        <h3>Setup in minutes</h3>
-        <p>
-          Get started fast with hundreds of visual and cutomizable templated -
-          or creat your own
-        </p>
-        <!-- <img> -->
+        <div class="txt">
+          <h3>Visualize work with Views</h3>
+          <p>View data as a kanban, danban, dashboard and more</p>
+        </div>
+        <img :src="require(`@/pics/picture3.png`)" />
       </section>
-
+      <section class="more-info">
+        <div class="imgs">
+          <img class="phone-pic" :src="require(`@/pics/picture6.jpg`)" />
+          <img class="phone-pic" :src="require(`@/pics/picture7.jpg`)" />
+        </div>
+        <div class="txt">
+          <h3>Setup in minutes</h3>
+          <p>
+            Get started fast with hundreds of visual and cutomizable templated -
+            or creat your own even from your phone
+          </p>
+        </div>
+      </section>
       <section class="trial">
         <h3>Try Tasker for your team</h3>
-        <p>14-day free trial | No credit card needed</p>
-        <button>Get Started</button>
+        <h5>14-day free trial | No credit card needed</h5>
+        <router-link to="/home">Get Started</router-link>
       </section>
       <section class="links">
         <a>About Us</a>
@@ -62,6 +71,7 @@
         <a>Terms and privacy</a>
         <a>Product overview</a>
       </section>
+      <section class="logo">Tasker</section>
     </section>
   </section>
 </template>
