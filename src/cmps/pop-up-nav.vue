116a6731f62bc6a563dbb535938b9965d3c1a095
SundosGutty <sundosgutty@gmail.com>
2021-12-03 02:11:54 +0200

    pop us menu

---
+++ src/cmps/pop-up-nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <div>
+      <h1>Workspace</h1>
+      <el-select>
+        <el-option>
+          <input type="text" />
+        </el-option>
+        <h1>Favorites</h1>
+      </el-select>
+    </div>
+    <div>
+      <h1>My workspace</h1>
+      <a>Add workspace</a>
+    </div>
+  </section>
+</template>
+
+
+
+<script>
+export default {};
+</script>
--- src/pages/workspace.vue
+++ src/pages/workspace.vue
@@ -1,5 +1,6 @@
 <template>
-  <section class="sub-workspace flex" >
+  <section class="sub-workspace flex">
+    <pop-up-nav class="pop-up-nav"></pop-up-nav>
     <section class="workspace-container">
       <board-header :board="currBoard" />
       <task-actions-nav />
@@ -15,6 +16,7 @@ import boardFilter from "@/cmps/board-filter.vue";
 import boardHeader from "@/cmps/board-header.vue";
 import taskActionsNav from "@/cmps/task-actions-nav.vue";
 import boardDetails from "@/cmps/board/board-details.vue";
+import popUpNav from "@/cmps/pop-up-nav.vue"
 
 export default {
   name: "workspace",
@@ -23,6 +25,7 @@ export default {
     boardDetails,
     boardHeader,
     taskActionsNav,
+    popUpNav
   },
   props: [],
   data() {
@@ -32,8 +35,10 @@ export default {
     };
   },
   async created() {
-     this.boards = this.$store.dispatch({type: "loadBoards", currBoardIdx: this.currBoardIdx,
-      });
+    this.boards = this.$store.dispatch({
+      type: "loadBoards",
+      currBoardIdx: this.currBoardIdx,
+    });
   },
   methods: {},
   computed: {
